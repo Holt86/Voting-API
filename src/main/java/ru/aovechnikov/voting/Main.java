a7fb084c73d1d59e1e832b0dedb2bbdb776ba924
Holt86 <aleksandr_986@inbox.ru>
2018-01-09 18:14:49 +0300

    readme

---
+++ src/main/java/ru/aovechnikov/voting/Main.java
@@ -0,0 +1,12 @@
+package ru.aovechnikov.voting;
+
+/**
+ * @author - A.Ovechnikov
+ * @date - 09.01.2018
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Voting!!!");
+    }
+}
